816cc5d87d73e305d017dd7aa5ab0b269c8633d0
Bogdan Patseev <patseev1987@gmail.com>
2024-06-02 13:03:54 +0300

    init commit

---
+++ src/main/java/ru/patseev/jaws_server/JawsServerApplication.java
@@ -0,0 +1,13 @@
+package ru.patseev.jaws_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JawsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JawsServerApplication.class, args);
+    }
+
+}
